736da11191dcac37ad9675e7e294eb5d8059352a
Harinath-0704 <m.harinath0103@gmail.com>
2026-08-13 11:48:41 +0530

    Update Exp-7.py

--- Lab Experiments/Exp-7.py
+++ Lab Experiments/Exp-7.py
@@ -1,3 +1,6 @@
+"""7) A delivery robot operates in a warehouse with predefined delivery points. Using Bellman
+equations, compute the state-value function for navigating to each delivery point.
+Implement this in Python and visualize the value function for different policies."""
 import numpy as np
 import matplotlib.pyplot as plt
 
